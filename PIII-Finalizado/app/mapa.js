edf371601d39ae1e8a7843381b24a11fe777a45b
LuizPauloAlves <76922545+LuizPauloAlves@users.noreply.github.com>
2024-05-05 20:10:03 -0300

    projeto Integrador 3 V1

---
+++ PIII-Finalizado/app/mapa.js
@@ -0,0 +1,39 @@
+let map;
+
+function initMap() {
+  const configuracao = {
+    center: { lat: -22.6112963189389, lng: -45.1773373236332 },
+    zoom: 14,
+    mapTypeId: "roadmap", // Corrigido: mapTypeId com "d" minúsculo
+    mapTypeControl: false,
+    streetViewControl: false,
+    styles: [
+      {
+        featureType: "poi",
+        stylers: [{ visibility: "off" }],
+      },
+    ],
+  };
+
+  map = new google.maps.Map(document.querySelector(".mapas"), configuracao);
+  verMarcadores(); // Chamar a função para exibir os marcadores
+}
+
+async function verDados() {
+  const conexao = await fetch("https://dengue-em-foco-api-889c311e3d33.herokuapp.com/v1/locations/");
+  const conexaoConvertida = await conexao.json();
+
+  return conexaoConvertida;
+}
+
+function createMarker(latitude, longitude) {
+  new google.maps.Marker({
+    position: { lat: latitude, lng: longitude },
+    map: map,
+  });
+}
+
+async function verMarcadores() {
+  const dados = await verDados();
+  dados.forEach((element) => createMarker(element.latitude, element.longitude));
+}
